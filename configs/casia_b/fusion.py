5380dedebdf2b5d7726f2d4103f1545662f7d54c
Duc-Tai Truong <taitruong256@gmail.com>
2026-07-01 13:50:17 +0800

    feat: add early fusion

--- configs/casia_b/fusion.py
+++ configs/casia_b/fusion.py
@@ -2,20 +2,28 @@ graph = 'coco'
 num_classes = 74
 work_dir = f'./work_dirs/casia_b/fusion_new'
 
-# Early fusion of the 6 default CASIA-B streams:
-# joint, bone, kbone, joint motion, bone motion, kbone motion.
-feats = ['j', 'b', 'k', 'jm', 'bm', 'km']
+# Early multi-branch fusion
+feats = ['j', 'b', 'k', 'jm']
 
 model = dict(
     type='RecognizerGCN',
     backbone=dict(
         type='ProtoGCN',
-        in_channels=18,
+        in_channels=12,
+        base_channels=64,
+        num_stages=4,
+        inflate_stages=[1, 3],
+        down_stages=[1, 3],
+        gcn_use_view=False,
+        gcn_use_euclid=False,
         num_prototype=300,
         view_num=11,
+        multi_branch=True,
+        multi_branch_stages=2,
+        branch_in_channels=3,
         tcn_ms_cfg=[(3, 1), (3, 2), (3, 3), (3, 4), ('max', 3), '1x1'],
         graph_cfg=dict(layout=graph, mode='random', num_filter=8, init_off=.04, init_std=.02)),
-    cls_head=dict(type='SimpleHead', joint_cfg=graph, num_classes=num_classes, in_channels=384, weight=0.2),
+    cls_head=dict(type='SimpleHead', joint_cfg=graph, num_classes=num_classes, in_channels=256, weight=0.2),
     view_loss_weight=1.0,
     test_cfg=dict(feat_ext=True, pool_opt='nmtv'))
 
@@ -26,21 +34,21 @@ test_ann_file = 'data/casia-b/casia-b_pose_test.csv'
 train_pipeline = [
     dict(type='RandomRot', theta=0.2),
     dict(type='GenSkeFeat', dataset=graph, feats=feats),
-    dict(type='UniformSampleDecode', clip_len=100),
+    dict(type='UniformSampleDecode', clip_len=60),
     dict(type='FormatGCNInput', num_person=1),
     dict(type='Collect', keys=['keypoint', 'label'], meta_keys=['subject', 'condition', 'view', 'sequence', 'frame_dir']),
     dict(type='ToTensor', keys=['keypoint'])
 ]
 val_pipeline = [
     dict(type='GenSkeFeat', dataset=graph, feats=feats),
-    dict(type='UniformSampleDecode', clip_len=100, num_clips=1),
+    dict(type='UniformSampleDecode', clip_len=60, num_clips=1),
     dict(type='FormatGCNInput', num_person=1),
     dict(type='Collect', keys=['keypoint', 'label'], meta_keys=['subject', 'condition', 'view', 'sequence', 'frame_dir']),
     dict(type='ToTensor', keys=['keypoint'])
 ]
 test_pipeline = [
     dict(type='GenSkeFeat', dataset=graph, feats=feats),
-    dict(type='UniformSampleDecode', clip_len=100, num_clips=10),
+    dict(type='UniformSampleDecode', clip_len=60, num_clips=10),
     dict(type='FormatGCNInput', num_person=1),
     dict(type='Collect', keys=['keypoint', 'label'], meta_keys=['subject', 'condition', 'view', 'sequence', 'frame_dir']),
     dict(type='ToTensor', keys=['keypoint'])
@@ -58,7 +66,7 @@ data = dict(
 optimizer = dict(type='SGD', lr=0.025, momentum=0.9, weight_decay=0.0005, nesterov=True)
 optimizer_config = dict(grad_clip=None)
 lr_config = dict(policy='CosineAnnealing', min_lr=0, by_epoch=False)
-total_epochs = 200
+total_epochs = 150
 checkpoint_config = dict(interval=1, max_keep_ckpts=1, save_last=True)
 evaluation = dict(
     interval=1,
--- protogcn/models/gcns/protogcn.py
+++ protogcn/models/gcns/protogcn.py
@@ -104,57 +104,105 @@ class ProtoGCN(nn.Module):
                  data_bn_type='VC',
                  num_person=2,
                  pretrained=None,
+                 multi_branch=False,
+                 multi_branch_stages=2,
+                 branch_in_channels=3,
                  **kwargs):
         super().__init__()
 
         self.graph = Graph(**graph_cfg)
         A = torch.tensor(self.graph.A, dtype=torch.float32, requires_grad=False)
         self.data_bn_type = data_bn_type
+        self.multi_branch = multi_branch
+        self.multi_branch_stages = multi_branch_stages
+        self.branch_in_channels = branch_in_channels
         self.kwargs = kwargs
+        self.num_person = num_person
 
-        if data_bn_type == 'MVC':
-            self.data_bn = nn.BatchNorm1d(num_person * in_channels * A.size(1))
-        elif data_bn_type == 'VC':
-            self.data_bn = nn.BatchNorm1d(in_channels * A.size(1))
+        if self.multi_branch:
+            if in_channels % branch_in_channels != 0:
+                raise ValueError(
+                    f'multi_branch=True expects in_channels to be divisible by '
+                    f'branch_in_channels, got in_channels={in_channels} and '
+                    f'branch_in_channels={branch_in_channels}.'
+                )
+            self.branch_num = in_channels // branch_in_channels
+            self.branch_data_bn = nn.ModuleList([
+                self._build_data_bn(num_person, branch_in_channels, A.size(1))
+                for _ in range(self.branch_num)
+            ])
         else:
-            self.data_bn = nn.Identity()
+            self.branch_num = 1
+            self.data_bn = self._build_data_bn(num_person, in_channels, A.size(1))
 
         num_prototype = kwargs.pop('num_prototype', 100)
-        lw_kwargs = [cp.deepcopy(kwargs) for i in range(num_stages)]
-        for k, v in kwargs.items():
-            if isinstance(v, tuple) and len(v) == num_stages:
-                for i in range(num_stages):
-                    lw_kwargs[i][k] = v[i]
-        lw_kwargs[0].pop('tcn_dropout', None)
-        lw_kwargs[0].pop('g1x1', None)
-        lw_kwargs[0].pop('gcn_g1x1', None)
+        self.stem_kwargs = cp.deepcopy(kwargs)
+        self.stem_kwargs.pop('tcn_dropout', None)
+        self.stem_kwargs.pop('g1x1', None)
+        self.stem_kwargs.pop('gcn_g1x1', None)
+
+        def _expand_stage_kwargs(stage_count):
+            stage_kwargs = [cp.deepcopy(kwargs) for _ in range(stage_count)]
+            for k, v in kwargs.items():
+                if isinstance(v, tuple) and len(v) == stage_count:
+                    for i in range(stage_count):
+                        stage_kwargs[i][k] = v[i]
+            return stage_kwargs
 
         self.in_channels = in_channels
         self.base_channels = base_channels
         self.ch_ratio = ch_ratio
         self.inflate_stages = inflate_stages
         self.down_stages = down_stages
-        modules = []
-        if self.in_channels != self.base_channels:
-            modules = [GCN_Block(in_channels, base_channels, A.clone(), 1, residual=False, **lw_kwargs[0])]
+        if self.multi_branch:
+            shared_stage_kwargs = _expand_stage_kwargs(num_stages)
+            branch_stage_kwargs = _expand_stage_kwargs(self.multi_branch_stages)
+            self.branch_stem = nn.ModuleList([
+                GCN_Block(branch_in_channels, base_channels, A.clone(), 1, residual=False, **cp.deepcopy(self.stem_kwargs))
+                for _ in range(self.branch_num)
+            ])
+            self.branch_gcn = nn.ModuleList([
+                nn.ModuleList([
+                    GCN_Block(base_channels, base_channels, A.clone(), 1, **branch_stage_kwargs[i])
+                    for i in range(self.multi_branch_stages)
+                ])
+                for _ in range(self.branch_num)
+            ])
+            modules = []
+            inflate_times = 0
+            for i in range(1, num_stages + 1):
+                stride = 1 + (i in down_stages)
+                in_c = base_channels
+                if i in inflate_stages:
+                    inflate_times += 1
+                out_c = int(self.base_channels * self.ch_ratio ** inflate_times + EPS)
+                base_channels = out_c
+                modules.append(GCN_Block(in_c, out_c, A.clone(), stride, **shared_stage_kwargs[i - 1]))
+            self.num_stages = num_stages
+            self.gcn = nn.ModuleList(modules)
+        else:
+            lw_kwargs = _expand_stage_kwargs(num_stages)
+            modules = []
+            if self.in_channels != self.base_channels:
+                modules = [GCN_Block(in_channels, base_channels, A.clone(), 1, residual=False, **lw_kwargs[0])]
 
-        inflate_times = 0
-        down_times = 0
-        for i in range(2, num_stages + 1):
-            stride = 1 + (i in down_stages)
-            in_channels = base_channels
-            if i in inflate_stages:
-                inflate_times += 1
-            out_channels = int(self.base_channels * self.ch_ratio ** inflate_times + EPS)
-            base_channels = out_channels
-            modules.append(GCN_Block(in_channels, out_channels, A.clone(), stride, **lw_kwargs[i - 1]))
-            down_times += (i in down_stages)
+            inflate_times = 0
+            down_times = 0
+            for i in range(2, num_stages + 1):
+                stride = 1 + (i in down_stages)
+                in_channels = base_channels
+                if i in inflate_stages:
+                    inflate_times += 1
+                out_channels = int(self.base_channels * self.ch_ratio ** inflate_times + EPS)
+                base_channels = out_channels
+                modules.append(GCN_Block(in_channels, out_channels, A.clone(), stride, **lw_kwargs[i - 1]))
+                down_times += (i in down_stages)
 
-        if self.in_channels == self.base_channels:
-            num_stages -= 1
+            if self.in_channels == self.base_channels:
+                num_stages -= 1
 
-        self.num_stages = num_stages
-        self.gcn = nn.ModuleList(modules)
+            self.num_stages = num_stages
+            self.gcn = nn.ModuleList(modules)
         self.pretrained = pretrained
         
         out_channels = base_channels
@@ -165,9 +213,37 @@ class ProtoGCN(nn.Module):
         self.bn = build_norm_layer(norm_cfg, out_channels)[1]
         self.relu = nn.ReLU()
         
-        dim = self.base_channels * 4 
+        dim = out_channels
         self.prn = Prototype_Reconstruction_Network(dim, num_prototype)
-        
+
+    def _build_data_bn(self, num_person, channels, num_joints):
+        if self.data_bn_type == 'MVC':
+            return nn.BatchNorm1d(num_person * channels * num_joints)
+        if self.data_bn_type == 'VC':
+            return nn.BatchNorm1d(channels * num_joints)
+        return nn.Identity()
+
+    def _apply_data_bn(self, x, data_bn, channels):
+        N, M, T, V, C = x.size()
+        x = x.permute(0, 1, 3, 4, 2).contiguous()
+        if self.data_bn_type == 'MVC':
+            x = data_bn(x.view(N, M * V * channels, T))
+        else:
+            x = data_bn(x.view(N * M, V * channels, T))
+        x = x.view(N, M, V, channels, T).permute(0, 1, 3, 4, 2).contiguous()
+        return x.view(N * M, channels, T, V)
+
+    def _run_stage_stack(self, x, modules):
+        get_graph = []
+        view_logits_list = []
+        for module in modules:
+            x, gcl_graph = module(x)
+            get_graph.append(gcl_graph)
+            view_logits = getattr(module.gcn, 'last_view_logits', None)
+            if view_logits is not None:
+                view_logits_list.append(view_logits)
+        return x, get_graph, view_logits_list
+
     def init_weights(self):
         if isinstance(self.pretrained, str):
             self.pretrained = cache_checkpoint(self.pretrained)
@@ -176,24 +252,57 @@ class ProtoGCN(nn.Module):
     def forward(self, x):
         logger.debug("ProtoGCN.forward: input=%s", _shape(x))
         N, M, T, V, C = x.size()
-        x = x.permute(0, 1, 3, 4, 2).contiguous()
-        if self.data_bn_type == 'MVC':
-            x = self.data_bn(x.view(N, M * V * C, T))
+        if self.multi_branch:
+            branch_chunks = torch.chunk(x, self.branch_num, dim=-1)
+            branch_outputs = []
+            get_graph = []
+            view_logits_list = []
+            for branch_idx, branch_x in enumerate(branch_chunks):
+                branch_x = self._apply_data_bn(
+                    branch_x,
+                    self.branch_data_bn[branch_idx],
+                    self.branch_in_channels,
+                )
+                logger.debug("ProtoGCN.forward: branch=%d after_data_bn=%s", branch_idx, _shape(branch_x))
+                branch_modules = [self.branch_stem[branch_idx], *self.branch_gcn[branch_idx]]
+                branch_x, branch_graphs, branch_views = self._run_stage_stack(branch_x, branch_modules)
+                branch_outputs.append(branch_x)
+                get_graph.extend(branch_graphs)
+                view_logits_list.extend(branch_views)
+                logger.debug(
+                    "ProtoGCN.forward: branch=%d out=%s",
+                    branch_idx,
+                    _shape(branch_x),
+                )
+            x = torch.stack(branch_outputs, dim=0).sum(dim=0)
+            logger.debug("ProtoGCN.forward: fused_branches=%s", _shape(x))
+            x, shared_graphs, shared_views = self._run_stage_stack(x, self.gcn)
+            get_graph.extend(shared_graphs)
+            view_logits_list.extend(shared_views)
+            logger.debug(
+                "ProtoGCN.forward: shared_stages=%d out=%s",
+                len(shared_graphs),
+                _shape(x),
+            )
         else:
-            x = self.data_bn(x.view(N * M, V * C, T))
-        x = x.view(N, M, V, C, T).permute(0, 1, 3, 4, 2).contiguous().view(N * M, C, T, V)
-        logger.debug("ProtoGCN.forward: after_data_bn=%s", _shape(x))
+            x = x.permute(0, 1, 3, 4, 2).contiguous()
+            if self.data_bn_type == 'MVC':
+                x = self.data_bn(x.view(N, M * V * C, T))
+            else:
+                x = self.data_bn(x.view(N * M, V * C, T))
+            x = x.view(N, M, V, C, T).permute(0, 1, 3, 4, 2).contiguous().view(N * M, C, T, V)
+            logger.debug("ProtoGCN.forward: after_data_bn=%s", _shape(x))
 
-        get_graph = []
-        view_logits_list = []
-        for i in range(self.num_stages):
-            x, gcl_graph = self.gcn[i](x)
-            # N*M C V V
-            get_graph.append(gcl_graph)
-            view_logits = getattr(self.gcn[i].gcn, 'last_view_logits', None)
-            if view_logits is not None:
-                view_logits_list.append(view_logits)
-            logger.debug("ProtoGCN.forward: stage=%d x=%s graph=%s", i, _shape(x), _shape(gcl_graph))
+            get_graph = []
+            view_logits_list = []
+            for i in range(self.num_stages):
+                x, gcl_graph = self.gcn[i](x)
+                # N*M C V V
+                get_graph.append(gcl_graph)
+                view_logits = getattr(self.gcn[i].gcn, 'last_view_logits', None)
+                if view_logits is not None:
+                    view_logits_list.append(view_logits)
+                logger.debug("ProtoGCN.forward: stage=%d x=%s graph=%s", i, _shape(x), _shape(gcl_graph))
         
         x = x.reshape((N, M) + x.shape[1:])
         c_graph = x.size(2)
