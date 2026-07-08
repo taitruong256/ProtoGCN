17229878e01fad6cf1c79bffddfe8095298122d5
Duc-Tai Truong <taitruong256@gmail.com>
2026-07-08 21:15:42 +0800

    fix: fix some bug in config

--- protogcn/apis/train.py
+++ protogcn/apis/train.py
@@ -237,7 +237,8 @@ def train_model(model,
     runner.register_training_hooks(cfg.lr_config, optimizer_config,
                                    cfg.checkpoint_config, cfg.log_config,
                                    cfg.get('momentum_config', None))
-    runner.register_hook(DistSamplerSeedHook())
+    if not use_iter_runner:
+        runner.register_hook(DistSamplerSeedHook())
 
     eval_hook = None
     if validate:
