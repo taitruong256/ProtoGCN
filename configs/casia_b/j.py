12da1f6cc148f5491e6040dd757e9a1a5c017dd6
Duc-Tai Truong <taitruong256@gmail.com>
2026-07-21 15:32:28 +0800

    fix: edit config

--- configs/casia_b/j.py
+++ configs/casia_b/j.py
@@ -1,15 +1,19 @@
 modality = 'j'
 graph = 'coco'
 num_classes = 74 
-work_dir = f'./work_dirs/casia_b/j_new'
+work_dir = f'./work_dirs/casia_b/j_new_4stage_48channel'
 
 model = dict(
     type='RecognizerGCN',
     backbone=dict(
         type='ProtoGCN',
         in_channels=3,
-        num_prototype=300,
+        num_prototype=200,
         view_num=11,
+        base_channels=48,
+        num_stages=4,
+        inflate_stages=[3, 4],
+        down_stages=[3, 4],
         tcn_ms_cfg=[(3, 1), (3, 2), (3, 3), (3, 4), ('max', 3), '1x1'],
         graph_cfg=dict(layout=graph, mode='random', num_filter=8, init_off=.04, init_std=.02)),
     cls_head=dict(type='SimpleHead', joint_cfg=graph, num_classes=num_classes, in_channels=384, weight=0.2),
@@ -17,27 +21,27 @@ model = dict(
     test_cfg=dict(feat_ext=True, pool_opt='nmtv'))
 
 dataset_type = 'CasiaBGaitDataset'
-train_ann_file = 'data/casia-b/casia-b_pose_train.csv'
+train_ann_file = 'data/casia-b/casia-b_pose_train_valid.csv'
 val_ann_file = 'data/casia-b/casia-b_pose_valid.csv'
 test_ann_file = 'data/casia-b/casia-b_pose_test.csv'
 train_pipeline = [
     dict(type='RandomRot', theta=0.2),
     dict(type='GenSkeFeat', dataset=graph, feats=[modality]),
-    dict(type='UniformSampleDecode', clip_len=100),
+    dict(type='UniformSampleDecode', clip_len=60),
     dict(type='FormatGCNInput', num_person=1),
     dict(type='Collect', keys=['keypoint', 'label'], meta_keys=['subject', 'condition', 'view', 'sequence', 'frame_dir']),
     dict(type='ToTensor', keys=['keypoint'])
 ]
 val_pipeline = [
     dict(type='GenSkeFeat', dataset=graph, feats=[modality]),
-    dict(type='UniformSampleDecode', clip_len=100, num_clips=1),
+    dict(type='UniformSampleDecode', clip_len=60, num_clips=1),
     dict(type='FormatGCNInput', num_person=1),
     dict(type='Collect', keys=['keypoint', 'label'], meta_keys=['subject', 'condition', 'view', 'sequence', 'frame_dir']),
     dict(type='ToTensor', keys=['keypoint'])
 ]
 test_pipeline = [
     dict(type='GenSkeFeat', dataset=graph, feats=[modality]),
-    dict(type='UniformSampleDecode', clip_len=100, num_clips=10),
+    dict(type='UniformSampleDecode', clip_len=60, num_clips=10),
     dict(type='FormatGCNInput', num_person=1),
     dict(type='Collect', keys=['keypoint', 'label'], meta_keys=['subject', 'condition', 'view', 'sequence', 'frame_dir']),
     dict(type='ToTensor', keys=['keypoint'])
@@ -55,7 +59,7 @@ data = dict(
 optimizer = dict(type='SGD', lr=0.025, momentum=0.9, weight_decay=0.0005, nesterov=True)
 optimizer_config = dict(grad_clip=None)
 lr_config = dict(policy='CosineAnnealing', min_lr=0, by_epoch=False)
-total_epochs = 200
+total_epochs = 5
 checkpoint_config = dict(interval=1, max_keep_ckpts=1, save_last=True)
 evaluation = dict(
     interval=1,
